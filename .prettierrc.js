4c9c969a7a4b14817a25cba2716a9d22068dd81f
ambar <ambar.lee@gmail.com>
2022-11-10 08:56:32 +0800

    update prettier

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('prettier').Options}
- * @see https://prettier.io/docs/en/options.html
- */
-module.exports = {
-  semi: false,
-  singleQuote: true,
-  bracketSpacing: false,
-}
